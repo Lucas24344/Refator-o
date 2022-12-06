faf201a69b5416713960efdd2f9dc4e6a89efe20
Lucas24344 <lucas.sousa.gomes08@aluno.ifce.edu.br>
2022-12-06 18:13:14 -0300

    Commit 17

--- src/atividade/TextStatement.java
+++ src/atividade/TextStatement.java
@@ -2,24 +2,22 @@ package atividade;
 import java.util.Enumeration;
 
 public class TextStatement extends Statement {
-	 public String value(Customer aCustomer) {
-		 Enumeration rentals = aCustomer.getRentals();
-	     String result = "Rental Record for " + aCustomer.getName() +
-	     "\n";
-	     while (rentals.hasMoreElements()) {
-	        Rental each = (Rental) rentals.nextElement();
-	        result += "\t" + each.getMovie().getTitle()+ "\t" +
-	        String.valueOf(each.getCharge()) + "\n";
-	     }
-	     result += "Amount owed is " +
-	     String.valueOf(aCustomer.getTotalCharge()) + "\n";
-	     result += "You earned " +
-	     String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
-	     " frequent renter points";
-	     return result;
-	  }
-		
-	
+	public String value(Customer aCustomer) {
+		Enumeration rentals = aCustomer.getRentals();
+		String result = "<H1>Rentals for <EM>" + aCustomer.getName() +
+			"</EM></H1><P>\n";
+		while (rentals.hasMoreElements()) {
+			Rental each = (Rental) rentals.nextElement();
+		    result += each.getMovie().getTitle()+ ": " +
+		    String.valueOf(each.getCharge()) + "<BR>\n";
+		}
+		result += "<P>You owe <EM>" +
+		String.valueOf(aCustomer.getTotalCharge()) + "</EM><P>\n";
+		result += "On this rental you earned <EM>" + 
+		String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
+		"</EM> frequent renter points<P>";
+		return result;
+	}
 }
 
 
